edcd3bce071f0f1bc847bca7d5ec432417baa44c
Santiago <126203646+SMartinGonzalez@users.noreply.github.com>
2025-02-28 17:24:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ let oneEuroIs = {
     "GBP": 0.87,
 }
 
+
 const fromEuroToDollar = function(valueInEuro){
     let valueInDollar = valueInEuro * 1.07;
     return valueInDollar;
 }
 
 const fromDollarToYen = function(valueInDollar){
-    let valueInYen = valueInDollar * 149.27;
+    let valueInEuro = valueInDollar / oneEuroIs.USD;
+    let valueInYen = valueInEuro * oneEuroIs.JPY;
     return valueInYen;
 }
 
 const fromYenToPound = function(valueInYen){
-    let valueInPound = valueInYen * 0.0053;
+   let valueInEuro = valueInYen / oneEuroIs.JPY;
+    let valueInPound = valueInEuro * oneEuroIs.GBP;
     return valueInPound;
 }
 

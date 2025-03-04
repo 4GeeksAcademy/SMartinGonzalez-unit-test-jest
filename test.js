309fec2e9d6eaa350ead874e87ddf5060bd29169
Santiago <126203646+SMartinGonzalez@users.noreply.github.com>
2025-03-04 17:56:17 +0000

    commit

--- test.js
+++ test.js
@@ -1,20 +1,19 @@
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
+
 test("One euro should be 1.07 dollars", function(){
-    const {fromEuroToDollar} = require('./app.js');
     const dollars = fromEuroToDollar(3.5);
     const expected = 3.5 * 1.07;
-    expect(fromEuroToDollar(3.5)).toBe(3.745);
-})
+    expect(dollars).toBe(expected);
+});
 
-test("One dollar should be 149.27 yens", function(){
-    const {fromDollarToYen} = require('./app.js');
+test("One dollar should be 156.5 / 1.07 yens", function(){
     const yens = fromDollarToYen(3.5);
-    const expected = 3.5 * 149.27;
-    expect(fromDollarToYen(3.5)).toBe(522.445);
-})
+    const expected = (3.5 / 1.07) * 156.5; 
+    expect(yens).toBeCloseTo(expected, 2); 
+});
 
-test("One yen should be 0.0053 pounds", function(){
-    const {fromYenToPound} = require('./app.js');
+test("One yen should be 0.87 / 156.5 pounds", function(){
     const pounds = fromYenToPound(3.5);
-    const expected = 3.5 * 0.0053;
-    expect(fromYenToPound(3.5)).toBe(0.01855);
-})
+    const expected = (3.5 / 156.5) * 0.87; 
+    expect(pounds).toBeCloseTo(expected, 5);
+});
